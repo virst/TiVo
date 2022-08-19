6d88ba54ab374405ecbccd96ad12f9163c9809a4
Вирстюк Михаил <m.vi@live.com>
2022-08-19 20:46:33 +0500

    const size

--- Program.cs
+++ Program.cs
@@ -2,13 +2,15 @@
 using System.Diagnostics;
 using TiVo;
 
+const int size = 1_000_000;
+
 Console.WriteLine("Start!");
 var sw = Stopwatch.StartNew();
 
-FileGenerate fg = new FileGenerate(100_000);
+FileGenerate fg = new FileGenerate(size);
 var nf = fg.SaveFile();
 FileOrder fo = new FileOrder(nf);
-fo.Order(10_000);
+fo.Order(size / 10);
 
 sw.Stop();
 Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds} ms.");
