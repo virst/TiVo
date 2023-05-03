d5a050822f8a87d54d6fc62c7d06bf105f3ad392
Вирстюк Михаил <m.vi@live.com>
2023-05-03 23:53:59 +0500

    Чутка причесал

--- Program.cs
+++ Program.cs
@@ -2,15 +2,21 @@
 using System.Diagnostics;
 using TiVo;
 
-const int size = 1_000_000;
+int size = 1_000_000;
+int split = 10;
+if (args.Length > 0)
+    size = Convert.ToInt32(args[0]);
+if (args.Length > 1)
+    split = Convert.ToInt32(args[1]);
 
 Console.WriteLine("Start!");
 var sw = Stopwatch.StartNew();
 
 FileGenerate fg = new(size);
 var nf = fg.SaveFile();
+Console.WriteLine($"FileGenerate Elapsed: {sw.ElapsedMilliseconds} ms.");
 FileOrder fo = new(nf);
-var orderedFile = fo.Order(size / 10);
+var orderedFile = fo.Order(size / split);
 
 sw.Stop();
 Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds} ms.");
